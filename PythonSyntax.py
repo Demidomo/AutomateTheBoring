41c68abdc84d3fe75b726cef7c82974f1e75db33
slow_n_steady <34130768+Demidomo@users.noreply.github.com>
2022-05-19 13:44:20 -0700

    Create PythonSyntax.py

---
+++ PythonSyntax.py
@@ -0,0 +1,20 @@
+#this program says hello and asks for my name.
+
+print('hello world') print('What is your name?') #this asks for their name
+
+myName = input() 
+print('Nice to meet you, ' + myName) 
+print ('The length of your name is:') 
+print(len(myName)) 
+
+print('What is your age?') 
+  myAge = input() 
+print('You will be ' +str(int(myAge) + 1) + ' in a year.')
+
+42 == 42 True 
+4333 == 2 False 
+2 != 3 True 
+2 != 2 False
+
+spam != False True 
+True = 2 File "", line 1 SyntaxError: cannot assign to True
